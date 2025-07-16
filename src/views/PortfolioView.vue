f0ed3a2650bec5b6a909305daf2c046ec6fcbedc
Chu Sỹ Minh <minhaevchhtb@gmail.com>
2025-07-16 15:02:32 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -66,7 +66,7 @@ export default {
           tech: 'Python, OpenCV, Mediapepe',
           github: 'https://github.com/ChuSyMinh/Hands-Free-Gaming-Controller',
           demo: 'null'
-        },
+        }
       ]
     };
   }
